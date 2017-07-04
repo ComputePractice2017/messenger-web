446afc39c822d04c46b90c832be4d7eaac996cc9
Eugene Frolov <thefrolovSV@gmail.com>
2017-07-04 12:03:44 +0300

    Окно аворизации issues-3

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-
+import signup from '@/components/signup'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'signup',
+      component: signup
     }
   ]
 })
